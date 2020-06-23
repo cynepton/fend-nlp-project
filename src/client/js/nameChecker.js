92fe689795fb952d5356a57ebdb325c2fa60bc12
isaac <aderonmuisaac@gmail.com>
2020-06-23 15:55:13 +0100

    style: add my name

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -5,7 +5,8 @@ function checkForName(inputText) {
         "Janeway",
         "Kirk",
         "Archer",
-        "Georgiou"
+        "Georgiou",
+        "Isaac"
     ]
 
     if(names.includes(inputText)) {
