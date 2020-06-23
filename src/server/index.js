23dd80c7ef6a3fa15c92559ad01f604d02d0295c
isaac <aderonmuisaac@gmail.com>
2020-06-23 09:44:33 +0100

    fix: adjust incorrect syntax

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 var AYLIENTextAPI = require('aylien_textapi');
 var textapi = new AYLIENTextAPI({
   application_id: process.env.API_ID,
-  application_key: rocess.env.API_KEY
+  application_key: process.env.API_KEY
 });
 
 const app = express()
