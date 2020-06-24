eff6055c3947f9b6118389e602941670e680a9be
isaac <aderonmuisaac@gmail.com>
2020-06-24 12:16:59 +0100

    chore: update webpack prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,20 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const WorkboxPlugin = require("workbox-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
     optimization: {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    output:{
+        libraryTarget : 'var',
+        library: 'Client'
     },
     module: {
         rules: [
@@ -20,9 +24,9 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            },
+                test:/\.scss$/,
+                use: [MiniCSSExtractPlugin.loader , 'css-loader', 'sass-loader']
+            }
         ]
     },
     plugins: [
@@ -30,6 +34,7 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({filename: 'outputStyle.css'})
+        new MiniCSSExtractPlugin({filename: '[name].css'}),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
