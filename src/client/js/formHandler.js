db316480304c6f53f0c4f0d1c30bf5e4271d3f28
isaac <aderonmuisaac@gmail.com>
2020-06-24 12:20:28 +0100

    feat: update formHandler to match config details

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,26 +2,31 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formContent = document.getElementById('name').value
+    let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:3000/data',{
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-            'Content-Type' : 'application/json',
-            'Access-Control-Allow-Origin' : "http://localhost:3000/"
-        },
-        body: JSON.stringify({test: formContent})
-    })
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('polarity').innerHTML=res.polarity
-        document.getElementById('subjectivity').innerHTML=res.subjectivity
-        document.getElementById('text').innerHTML = res.text
-        document.getElementById('polarity-confidence').innerHTML=res.polarity_confidence
-        document.getElementById('subjectivity-confidence').innerHTML=res.subjectivity_confidence
-    })
+
+    console.log(Client.checkForName(formText))
+    {
+        fetch('http://localhost:8081/data',{
+            method: "POST",
+            credentials: "same-origin",
+            headers: {
+                'Content-Type' : 'application/json',
+                'Access-Control-Allow-Origin' : "http://localhost:8081/"
+            },
+            body: JSON.stringify({test: formText})
+        })
+        .then(res => res.json())
+        .then(function(res){
+            document.getElementById('polarity').innerHTML=res.polarity
+            document.getElementById('subjectivity').innerHTML=res.subjectivity
+            document.getElementById('polarity-confidence').innerHTML=res.polarity_confidence
+            document.getElementById('subjectivity-confidence').innerHTML=res.subjectivity_confidence
+            document.getElementById('text').innerHTML = res.text
+        })
+    }
 }
+
 export { handleSubmit }
