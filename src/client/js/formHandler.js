554f9af1824750cb2fac08f8cd8817769f1fc957
isaac <aderonmuisaac@gmail.com>
2020-06-24 11:27:57 +0100

    feat: add fetch data and update ui features

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,15 +2,26 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formContent = document.getElementById('name').value
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:3000/data',{
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            'Content-Type' : 'application/json',
+            'Access-Control-Allow-Origin' : "http://localhost:3000/"
+        },
+        body: JSON.stringify({test: formContent})
+    })
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('polarity').innerHTML=res.polarity
+        document.getElementById('subjectivity').innerHTML=res.subjectivity
+        document.getElementById('text').innerHTML = res.text
+        document.getElementById('polarity-confidence').innerHTML=res.polarity_confidence
+        document.getElementById('subjectivity-confidence').innerHTML=res.subjectivity_confidence
     })
 }
-
 export { handleSubmit }
